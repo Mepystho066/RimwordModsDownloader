d291260ab9b749ed808826f479636560cfdf3a6f
mepystho066 <samuelarango1206@gmail.com>
2024-10-01 17:27:10 -0500

    Update 0.2

--- App/logic/linkProgressing.py
+++ App/logic/linkProgressing.py
@@ -3,54 +3,13 @@ import aiohttp
 from bs4 import BeautifulSoup
 import time
 import random
-class rimworld:
-    def __init__(self, link):
-        self.link = link
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)  Chrome/91.0.4472.124'}
-        self.params={
-      #'api_key': 'YOUR_API_KEY',
-      #'url': 'http://url', ## Cloudflare protected website 
-      'bypass': 'cloudflare_level_1',}
-    async def fetchContent(self):
-        #time.sleep(random.randint(3,6))
-        await asyncio.sleep(random.uniform(3, 5))
-        async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:  
-           # time.sleep(random.randint(1,2))
-            async with session.get(self.link, headers=self.headers,params=self.params) as response:
-                return await response.text()
 
-    async def stractModName(self):
-        time.sleep(random.randint(1,2))
-        webContent = await self.fetchContent()
-        visibleWebContent = BeautifulSoup(webContent, 'html.parser')
-        h1_tag = visibleWebContent.find('h1')
-        return h1_tag
-
-    async def stractImage(self):
-        time.sleep(random.randint(1,2))
-        webContent = await self.fetchContent()
-        getImage = BeautifulSoup(webContent, 'html.parser')
-        enlaces_mega = getImage.find_all('img', src=lambda src: src and ('.jpg' in src))# or '.jpg' in src ) )
-        return [img['src'] for img in enlaces_mega]
-
-    async def extract_mega_url(self):
-        time.sleep(random.randint(1,2))
-        webContent= await self.fetchContent()
-        visibleWebContent = BeautifulSoup(webContent, 'html.parser')
-        enlaces_mega =visibleWebContent.find_all('a' , href= lambda href : href and 'download' in href)
-        return [a['href'] for a in enlaces_mega]
-
-    async def mostrar(self):
-        mod_name_task = self.stractModName()
-        image_urls_task = self.stractImage()
-        mega = self.extract_mega_url()
-        mod_name = await mod_name_task
-        image_urls = await image_urls_task
-        linkMega = await mega
-        return mod_name,image_urls,linkMega
-
+"""
+Cada clase define una pagina web
+"""
 
 class topMods:
+    
     def __init__(self, link):
         self.link = link
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)  Chrome/91.0.4472.124'}
@@ -58,12 +17,14 @@ class topMods:
       #'api_key': 'YOUR_API_KEY',
       #'url': 'http://url', ## Cloudflare protected website 
       'bypass': 'cloudflare_level_1',}
-    async def fetchContent(self):
+    async def fetchContent(self,linkData = None ):
+        if linkData is None:
+            linkData = self.link
         #time.sleep(random.randint(3,6))
         await asyncio.sleep(random.uniform(3, 5))
         async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:  
            # time.sleep(random.randint(1,2))
-            async with session.get(self.link, headers=self.headers,params=self.params) as response:
+            async with session.get(linkData, headers=self.headers,params=self.params) as response:
                 return await response.text()
 
     async def stractModName(self):
@@ -72,6 +33,7 @@ class topMods:
         visibleWebContent = BeautifulSoup(webContent, 'html.parser')
         h1_tag = visibleWebContent.find('h1')
         return h1_tag.text
+
     async def stractImage(self):
         time.sleep(random.randint(1,2))
         webContent = await self.fetchContent()
@@ -84,71 +46,51 @@ class topMods:
         webContent= await self.fetchContent()
         visibleWebContent = BeautifulSoup(webContent, 'html.parser')
         enlaces_mega =visibleWebContent.find_all('a' , href= lambda href : href and 'modsfire' in href)
-        return [a['href'] for a in enlaces_mega]
+        link = [a['href'] for a in enlaces_mega]
+        #Actualizar todos los componentes de link para que ya no sean link[0]
+        return link
+
+    async def extract_mega_url2(self):
+        time.sleep(random.randint(1,2))
+        link = await self.extract_mega_url()
+        #print("depu" + link[0])
+        webContent= await self.fetchContent(link[0])
+        visibleWebContent = BeautifulSoup(webContent, 'html.parser')
+        button = visibleWebContent.find("a",{'class':'download-button'})
+        
+        linkStract = "https://modsfire.com"+button["href"]
+        
+        async with aiohttp.ClientSession() as session:
+            # Simulamos un POST, dependiendo de los datos requeridos
+            async with session.post(linkStract, data={'key': 'value'}) as response:
+                content = await response.text() 
+                info = BeautifulSoup(content,'html.parser')
+                print(info)
+
+        #webContent2= await self.fetchContent(linkStract)
+        #visibleWebContent2 = BeautifulSoup(webContent2, 'html.parser')
+        #button2 = visibleWebContent.find("a",{'class':'download-button'})#, href= lambda href : href and '/d/' in href)
+        #print(button2)
+      
+        return ""# finalLink
+
 
     async def mostrar(self):
         complemento = self.link.split("/")
         mod_name_task = self.stractModName()
         image_urls_task = self.stractImage()
         mega = self.extract_mega_url()
+        mega2= self.extract_mega_url2()
         mod_name = await mod_name_task
         image_urls = await image_urls_task
         linkMega = await mega
+        linkMega2 = await mega2
+        print(linkMega2)
         name = mod_name.replace("\n","")
         name =  name.replace("        ","")
         contenido = f'https://{complemento[2]}{image_urls[0]}'
-        #print(contenido)
-        return name,contenido,linkMega[0]
-
-class Smods:
-    def __init__(self, link):
-        self.link = link
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)  Chrome/91.0.4472.124'}
-        self.params={
-      #'api_key': 'YOUR_API_KEY',
-      #'url': 'http://url', ## Cloudflare protected website 
-      'bypass': 'cloudflare_level_1',}
-    async def fetchContent(self):
-        #time.sleep(random.randint(3,6))
-        await asyncio.sleep(random.uniform(3, 5))
-        async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:  
-           # time.sleep(random.randint(1,2))
-            async with session.get(self.link, headers=self.headers,params=self.params) as response:
-                return await response.text()
-
-    async def stractModName(self):
-        time.sleep(random.randint(1,2))
-        webContent = await self.fetchContent()
-        visibleWebContent = BeautifulSoup(webContent, 'html.parser')
-        h1_tag = visibleWebContent.find('h1')
-        return h1_tag
-
-    async def stractImage(self):
-        time.sleep(random.randint(1,2))
-        webContent = await self.fetchContent()
-        getImage = BeautifulSoup(webContent, 'html.parser')
-        enlaces_mega = getImage.find_all('img', src=lambda src: src and ('.jpg' in src))# or '.jpg' in src ) )
-        return [img['src'] for img in enlaces_mega]
-
-    async def extract_mega_url(self):
-        time.sleep(random.randint(1,2))
-        webContent= await self.fetchContent()
-        visibleWebContent = BeautifulSoup(webContent, 'html.parser')
-        enlaces_mega =visibleWebContent.find_all('a' , href= lambda href : href and 'download' in href)
-        return [a['href'] for a in enlaces_mega]
-
-    async def mostrar(self):
-        mod_name_task = self.stractModName()
-        image_urls_task = self.stractImage()
-        mega = self.extract_mega_url()
-        mod_name = await mod_name_task
-        image_urls = await image_urls_task
-        linkMega = await mega
-        return mod_name,image_urls,linkMega
-
-
-
-
+        
+        return name,contenido,linkMega[0], linkMega2
 
 async def LinkList(lista:[]):
     tasks = [StractLink(link).mostrar() for link in lista]
@@ -164,8 +106,8 @@ async def run_program(link):
     print("Tiempo total:", tiempo_final - tiempo_inicial)
 """
 
-#if __name__ =="__main__":
-#    info = topMods("https://top-mods.com/mods/rimworld/other/12995-directly-abandon-settlement.html")    
-#    
-#    info2 = asyncio.run(info.mostrar())
-#    print(info2)
+if __name__ =="__main__":
+    info = topMods("https://top-mods.com/mods/rimworld/other/13165-ebsg-framework.html")    
+    
+    info2 = asyncio.run(info.mostrar())
+    print(info2)
---
+++ App/logic/stractRim.py
@@ -0,0 +1,50 @@
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+import time
+import random
+class rimworld:
+    def __init__(self, link):
+        self.link = link
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)  Chrome/91.0.4472.124'}
+        self.params={
+      #'api_key': 'YOUR_API_KEY',
+      #'url': 'http://url', ## Cloudflare protected website 
+      'bypass': 'cloudflare_level_1',}
+    async def fetchContent(self):
+        #time.sleep(random.randint(3,6))
+        await asyncio.sleep(random.uniform(3, 5))
+        async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:  
+           # time.sleep(random.randint(1,2))
+            async with session.get(self.link, headers=self.headers,params=self.params) as response:
+                return await response.text()
+
+    async def stractModName(self):
+        time.sleep(random.randint(1,2))
+        webContent = await self.fetchContent()
+        visibleWebContent = BeautifulSoup(webContent, 'html.parser')
+        h1_tag = visibleWebContent.find('h1')
+        return h1_tag
+
+    async def stractImage(self):
+        time.sleep(random.randint(1,2))
+        webContent = await self.fetchContent()
+        getImage = BeautifulSoup(webContent, 'html.parser')
+        enlaces_mega = getImage.find_all('img', src=lambda src: src and ('.jpg' in src))# or '.jpg' in src ) )
+        return [img['src'] for img in enlaces_mega]
+
+    async def extract_mega_url(self):
+        time.sleep(random.randint(1,2))
+        webContent= await self.fetchContent()
+        visibleWebContent = BeautifulSoup(webContent, 'html.parser')
+        enlaces_mega =visibleWebContent.find_all('a' , href= lambda href : href and 'download' in href)
+        return [a['href'] for a in enlaces_mega]
+
+    async def mostrar(self):
+        mod_name_task = self.stractModName()
+        image_urls_task = self.stractImage()
+        mega = self.extract_mega_url()
+        mod_name = await mod_name_task
+        image_urls = await image_urls_task
+        linkMega = await mega
+        return mod_name,image_urls,linkMega
